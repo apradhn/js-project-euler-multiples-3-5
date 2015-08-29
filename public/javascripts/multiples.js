bb2578127f36faa03d478b44e2cd51d1832d6430
apradhn <abhijit.pradhan6@gmail.com>
2015-08-29 14:11:10 -0400

    writes logic for #collect_multiples

--- public/javascripts/multiples.js
+++ public/javascripts/multiples.js
@@ -1,3 +1,11 @@
 function collectMultiples(limit) {
-  return []
+  var multiples = [];
+  var i = 1
+  while(i < limit){
+    if (i % 5 == 0 || i % 3 == 0) {
+      multiples.push(i);
+    };
+    i += 1;
+  }
+  return multiples;
 }
--- spec/javascripts/multiples_spec.js
+++ spec/javascripts/multiples_spec.js
@@ -2,4 +2,8 @@ describe("#collectMultiples(limit)", function() {
   it('collects the correct multiples of natural numbers below 10', function() {
     expect(collectMultiples(10)).toEqual([3,5,6,9]);
   });
+
+  it('returns the correct multiples of natural numbers below 100', function() {
+    expect(collectMultiples(100)).toEqual([3,5,6,9,10,12,15,18,20,21,24,25,27,30,33,35,36,39,40,42,45,48,50,51,54,55,57,60,63,65,66,69,70,72,75,78,80,81,84,85,87,90,93,95,96,99])
+  });
 });
